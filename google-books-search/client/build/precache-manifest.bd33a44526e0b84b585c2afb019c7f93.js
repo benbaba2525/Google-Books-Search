a1ee048e67ddb05682cd1d59b914776f2e21ffe0
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-08-24 20:22:01 -0700

    Added route models and controller

---
+++ google-books-search/client/build/precache-manifest.bd33a44526e0b84b585c2afb019c7f93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d0d89c6cd1decd4a569",
+    "url": "/static/css/main.6485d277.chunk.css"
+  },
+  {
+    "revision": "9d0d89c6cd1decd4a569",
+    "url": "/static/js/main.bac6723c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "15f024ac7807c06f0d3d",
+    "url": "/static/js/2.85a32209.chunk.js"
+  },
+  {
+    "revision": "2555f71f352bb8b30e6809a5408b8496",
+    "url": "/static/media/bookshelf.2555f71f.jpeg"
+  },
+  {
+    "revision": "4002e1e018d58764e57fe6309e95d817",
+    "url": "/index.html"
+  }
+];
